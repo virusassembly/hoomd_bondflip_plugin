33b65f2bc94ad1ca08d9e49d58d7a356b2faec43
sli032 <sli032@ucr.edu>
2022-07-28 16:42:23 +0800

    updata

---
+++ bondflip_plugin/bondflip_plugin/test-py/test_bondflip.py
@@ -0,0 +1,12 @@
+import hoomd;
+hoomd.context.initialize();
+import hoomd.bondflip_plugin;
+import unittest;
+import os;
+
+class test_bondflip(unittest.TestCase):
+    def test_constructor(self):
+        # python simple is given seperately
+        
+if __name__ == '__main__':
+    unittest.main(argv = ['test_bondflip.py', '-v'])
